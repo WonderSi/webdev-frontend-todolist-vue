56510335ea29df157f7cf76333bb7a6680ef0ef1
WonderSi <kirillkozlov00@gmail.com>
2025-11-06 10:40:40 +0700

    feat: create component Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,18 @@
 <template>
-
+    <header class="header">
+        <h1 class="header__name">TODO LIST</h1>
+        <div class="header__nav">
+            <SearchBar/>
+            <FiltersDropdown/>
+            <ThemeToggle/>            
+        </div>
+    </header>
 </template>
 
 <script setup>
-
+    import SearchBar from '@/components/SearchBar.vue'
+    import FiltersDropdown from '@/components/FiltersDropdown.vue'
+    import ThemeToggle from '@/components/ThemeToggle.vue'
 </script>
 
 <style scoped lang="scss">
