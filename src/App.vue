185417f564c63c65cb2479bdf8be0e1ff0f4a28a
WonderSi <kirillkozlov00@gmail.com>
2025-11-07 22:09:26 +0700

    refactor: change file App.vue (add component TodoList.vue)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
     <Header/>
+    <TodoList/> 
 </template>
 
 <script setup>
     import Header from '@/components/Header.vue';
+    import TodoList from '@/components/TodoList.vue';
 </script>
 
 <style scoped lang="scss">
